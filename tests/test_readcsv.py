0b72853a44179e910b9e99bdcffe4e0da3fa557e
Mathieu Moretti <mormmorm@hotmail.com>
2019-02-18 21:04:50 -0500

    Non fully functioning folder architecture

--- tests/test_readcsv.py
+++ tests/test_readcsv.py
@@ -2,28 +2,45 @@ import csv
 import os
 from os import walk
 
+from mlb_learning.player import Player
+from models.player import Player
+
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 root_dir = os.path.join(script_dir, '..')
 data_path = os.path.join(root_dir, 'data')
 
-
-def reader(filePath):
+def read_csv(filePath):
+    rows = []
     with open(filePath, newline='') as csvfile:
         playerreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in playerreader:
-            print(', '.join(row))
+            rows.append(row)
+            #print(', '.join(row))
+    
 
-def parser():
-    return []
+    raw_logs = list(filter(None, rows))
+    print(raw_logs)
+    return raw_logs
 
-
-
-def get_csv_files():
+def collect_csv_files():
     f = []
     for (dirpath, dirnames, filenames) in walk(data_path):
-        f.extend(filenames)        
+        f.extend([os.path.join(dirpath,x) for x in filenames])        
     return f
 
 
-for filename in get_csv_files():
-    reader(os.path.join(data_path, filename))
+for filepath in collect_csv_files():
+    # maybe use regex
+    filename = os.path.splitext(os.path.basename(filepath))[0]
+    tokens = filename.split('_')
+    first_name = tokens[0]
+    last_name = tokens[1]
+    espn_id = tokens[2]
+
+    player = models.Player()
+    player.first_name = first_name
+    player.last_name = last_name
+    player.espn_id = id
+
+    logs = read_csv(filepath)
+    gamelogs = logs[1:]
