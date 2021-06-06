2a246d1ee89078be37057afe769f8b5341c53d3c
Mathieu Moretti <mormmorm@hotmail.com>
2021-06-05 21:55:17 -0400

    Creating a setup file to have a python lib + dummy code to setup test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,8 @@
+from mlb_learning import myfunctions
 # content of test_sample.py
 def test_dummy():
     someVar = 2
     assert 2 == someVar
+
+def test_zero():
+    assert myfunctions.zero() == 0
