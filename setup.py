c0980c66c16a1f1e9aebe61f07f7ab7632ad9065
Mathieu Moretti <mormmorm@hotmail.com>
2021-06-12 15:44:46 -0400

    Move to static python .cfg file

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='mlb_learning',
-    packages=find_packages(include=['mlb_learning']),
-    version='0.0.1',
-    description='MLB Learning',
-    author='M.Moretti, S.Pradier',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==6.2.2'],    
+setup(  
 )
